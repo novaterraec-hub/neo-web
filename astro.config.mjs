5b8a3e0b55011229ffd44e69639c838b17741f73
ram <novaterraec@gmail.com>
2026-04-15 02:47:26 -0500

    Add React integration to Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
 
 export default defineConfig({
   site: 'https://novaterraec-hub.github.io',
   base: 'neo-web',
   output: 'static',
+  integrations: [react()],
 });
